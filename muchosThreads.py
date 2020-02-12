d7628eedbaa7c5d7e60c0020d32d4fbe07edcc9a
Emanuelr9 <emanuelr9@yahoo.com.ar>
2020-02-12 15:44:01 -0300

    Primer TP

--- muchosThreads.py
+++ muchosThreads.py
@@ -2,15 +2,41 @@ import threading
 import time
 import logging
 
+from tiempo import Contador
+
+cont=Contador()
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 # la función para usar para el thread
-def dormir():
-    time.sleep()
+def dormir(secs):
+    time.sleep(secs)
 
+cont=Contador()
 
+cont.iniciar()
+
+lista=[] #crea una lista vacia
 
 for i in range(10):
-    #crear un thead
+   #crear un thread  
+    t= threading.Thread(target=dormir, args=[1.5] )
     #lanzarlo
+    t.start()
+    lista.append(t)
+
+    #t.join()
+
+for thread in lista:
+    thread.join()
+
+cont.finalizar()
+cont.imprimir()
+
+
+
+
+
+
+
+   
--- operaNoConm.py
+++ operaNoConm.py
@@ -2,7 +2,54 @@ import threading
 import time
 import logging
 
+
+from tiempo import Contador
+
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+num=10
+#Escpeci de advertencia
+
+semaforo = threading.Semaphore(1)
+
+lock=threading.Lock()
+
+def sumarUno():
+
+    global num
+    global lock
+    
+    try:
+        num +=1
+       
+    finally:
+       lock.release() #¿qué pasa si comento este release y descomento el pass
 
 
+def multiplicarPorDos():
+
+    global num
+    global lock
+
+    lock.acquire()
+    
+    try:
+            num *=2          
+    finally:
+
+     lock.release()
+
+
+t1 = threading.Thread(target=sumarUno, name='Sumar')
+t2 = threading.Thread(target=multiplicarPorDos, name='Multiplicar')
+
+lock.acquire()
+
+t2.start()
+t1.start()
+
+
+t2.join()
+logging.info(f'valor final {num}')
+
+#print(num)
